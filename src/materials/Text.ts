34ee3c62d92575f31da4abdae926f4e378ffd2d9
Evan Suma Rosenberg <suma@umn.edu>
2022-09-18 08:39:52 -0500

    text rendering improvements

--- src/materials/Text.ts
+++ src/materials/Text.ts
@@ -2,119 +2,70 @@ import { Texture } from './Texture'
 
 export class Text extends Texture
 {
-    private text: string;
-    private fill: boolean;
-    private stroke: boolean;
+    public text: string;
+    public font: string;
+    public fillStyle: string;
+    public strokeStyle: string;
+    public backgroundStyle: string;
+    public align: CanvasTextAlign;
+    public baseline: CanvasTextBaseline;
+
+    public width: number;
+    public height: number;
+    public strokeWidth: number;
+
     private textCanvas: CanvasRenderingContext2D | null;
 
-    constructor(text: string, width: number, height: number, font = '24px monospace', align: CanvasTextAlign = 'center', 
-    baseline: CanvasTextBaseline = 'middle', fillStyle = 'black', strokeStyle = 'black', strokeWidth = 0)
+    constructor(text: string, width: number, height: number, font = '24px monospace', 
+                fillStyle = 'black', backgroundStyle = '', strokeStyle = '', strokeWidth = 1,
+                align: CanvasTextAlign = 'center', baseline: CanvasTextBaseline = 'middle')
     {
         super();
 
         this.text = text;
-        this.fill = fillStyle == '' ? false : true;
-        this.stroke = (strokeStyle ==  '' || strokeWidth == 0) ? false : true;
-
+        this.width = width;
+        this.height = height;
+        this.font = font;
+        this.fillStyle = fillStyle;
+        this.backgroundStyle = backgroundStyle;
+        this.strokeStyle = strokeStyle;
+        this.strokeWidth = strokeWidth;
+        this.align = align;
+        this.baseline = baseline;
+        
         this.textCanvas = document.createElement('canvas').getContext('2d');
-        if(this.textCanvas)
-        {
-            this.textCanvas.canvas.width = width;
-            this.textCanvas.canvas.height = height;
-            this.textCanvas.font = font;
-            this.textCanvas.textAlign = align;
-            this.textCanvas.textBaseline = baseline;
-            this.textCanvas.fillStyle = fillStyle;
-            this.textCanvas.strokeStyle = strokeStyle;
-            this.textCanvas.lineWidth = strokeWidth;
-        }
-
         this.updateTextureImage();
-    }
-
-    setText(text: string): void
-    {
-        this.text = text;
-    }
-
-    setSize(width: number, height: number): void
-    {
-        if(this.textCanvas)
-        {
-            this.textCanvas.canvas.width = width;
-            this.textCanvas.canvas.height = height;
-        }
-    }
-
-    setFont(font: string): void
-    {
-        if(this.textCanvas)
-        {
-            this.textCanvas.font = font;
-        }
-    }
-
-    setAlign(align: CanvasTextAlign): void
-    {
-        if(this.textCanvas)
-        {
-            this.textCanvas.textAlign = align;
-        }
-    }
-
-    setBaseline(baseline: CanvasTextBaseline): void
-    {
-        if(this.textCanvas)
-        {
-            this.textCanvas.textBaseline = baseline;
-        }
-    }
-
-    setFillStyle(fillStyle: string): void
-    {
-        this.fill = fillStyle == '' ? false : true;
-
-        if(this.textCanvas)
-        {
-            this.textCanvas.fillStyle = fillStyle;
-        }
-    }
-
-    setStrokeStyle(strokeStyle: string): void
-    {
-        this.stroke = strokeStyle ==  '' ? false : true;
-
-        if(this.textCanvas)
-        {
-            this.textCanvas.strokeStyle = strokeStyle;
-        }
-    }
-
-    setStrokeWidth(strokeWidth: number): void
-    {
-        this.stroke = strokeWidth == 0 ? false : true;
-
-        if(this.textCanvas)
-        {
-            this.textCanvas.lineWidth = strokeWidth;
-        }
     }
 
     public updateTextureImage(): void
     {
         if(this.textCanvas)
-        {
-            this.textCanvas.clearRect(0, 0, this.textCanvas.canvas.width, this.textCanvas.canvas.height);
+        {   
+            this.textCanvas.canvas.width = this.width;
+            this.textCanvas.canvas.height = this.height;
+            this.textCanvas.font = this.font;
+            this.textCanvas.textAlign = this.align;
+            this.textCanvas.textBaseline = this.baseline;
 
-            if(this.fill)
+            this.textCanvas.clearRect(0, 0, this.width, this.height);
+
+            if(this.backgroundStyle != '')
             {
-                this.textCanvas!.fillText(this.text, this.textCanvas.canvas.width / 2, this.textCanvas.canvas.height / 2);
+                this.textCanvas.fillStyle = this.backgroundStyle;
+                this.textCanvas.fillRect(0, 0, this.width, this.height);
             }
 
-            if(this.stroke)
+            if(this.fillStyle != '')
             {
-                console.log(this.textCanvas!.lineWidth);
-                this.textCanvas!.strokeText(this.text, this.textCanvas.canvas.width / 2, this.textCanvas.canvas.height / 2);
+                this.textCanvas.fillStyle = this.fillStyle;
+                this.textCanvas.fillText(this.text, this.width / 2, this.height / 2);
+            }
+
+            if(this.strokeStyle != '' && this.strokeWidth > 0)
+            {
+                this.textCanvas.strokeStyle = this.strokeStyle;
+                this.textCanvas.lineWidth = this.strokeWidth;
+                this.textCanvas.strokeText(this.text, this.width / 2, this.height / 2);
             }
 
             this.gl.activeTexture(this.gl.TEXTURE0 + this.id);
